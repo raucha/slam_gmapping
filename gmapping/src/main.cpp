c1c118133b25e556be875147771fe2c059cc79d9
Akio Shigekane <motorado519+git@gmail.com>
2016-01-02 13:15:31 +0900

    lint

--- gmapping/src/main.cpp
+++ gmapping/src/main.cpp
@@ -6,7 +6,7 @@
  * COMMONS PUBLIC LICENSE ("CCPL" OR "LICENSE"). THE WORK IS PROTECTED BY
  * COPYRIGHT AND/OR OTHER APPLICABLE LAW. ANY USE OF THE WORK OTHER THAN AS
  * AUTHORIZED UNDER THIS LICENSE OR COPYRIGHT LAW IS PROHIBITED.
- * 
+ *
  * BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE TO
  * BE BOUND BY THE TERMS OF THIS LICENSE. THE LICENSOR GRANTS YOU THE RIGHTS
  * CONTAINED HERE IN CONSIDERATION OF YOUR ACCEPTANCE OF SUCH TERMS AND
@@ -20,15 +20,12 @@
 
 #include "slam_gmapping.h"
 
-int
-main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
   ros::init(argc, argv, "slam_gmapping");
 
   SlamGMapping gn;
   gn.startLiveSlam();
   ros::spin();
 
-  return(0);
+  return (0);
 }
-
